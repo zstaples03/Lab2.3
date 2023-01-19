175f739a521dda5df381b42b46feb3fed1d4d028
Zachary Thomas Staples <zstaples@purdue.edu>
2023-01-19 15:01:27 -0500

    Math and coding = qquestion my major

---
+++ Lab2.3/Lab2.3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float A;
+	float B;
+	float X;
+	cout << "Hello my name is Zachary Staples and I am going to solve the equation" << endl;
+	cout << "Ax + B = 0" << endl;
+	cout << "For x" << endl;
+
+
+
+
+	cout << "Please enter a value for A:";
+	cin >> A;
+
+
+
+	cout << "Please enter a value for B:";
+	cin >> B;
+
+	cout << "" << endl;
+
+
+	cout << "Solving for" << A << "x+" << B << "=0 For X..." << endl;
+	cout << "" << endl;
+
+	cout << "The Answer is.." << endl;
+
+	X = -B / A;
+
+	cout << "X =" << X << endl;
+
+
+}
